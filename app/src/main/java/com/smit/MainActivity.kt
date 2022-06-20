72f8c9d9c743816ac15acc1a36f30ef4eb6aef67
smit0101 <smitkalkani9099@gmail.com>
2022-06-20 15:37:21 +0530

    updated dependencies

--- app/src/main/java/com/smit/MainActivity.kt
+++ app/src/main/java/com/smit/MainActivity.kt
@@ -12,9 +12,9 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.BasicTextField
@@ -37,6 +37,7 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
@@ -73,10 +74,15 @@ class MainActivity : ComponentActivity() {
                     ApiTesting()
 
 
+
                 }
             }
         }
     }
+}
+
+class A:ViewModel(){
+
 }
 
 @Composable
@@ -268,7 +274,7 @@ fun AnimateApp() {
 @Composable
 fun GridTest() {
     LazyVerticalGrid(
-        cells = GridCells.Adaptive(128.dp),
+        columns = GridCells.Adaptive(128.dp),
         contentPadding = PaddingValues(all = 20.dp),
         modifier = Modifier
             .background(Color.Black)
